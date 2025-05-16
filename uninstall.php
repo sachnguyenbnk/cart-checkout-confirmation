a78f280a66c03b9dc94b9755eb88a80fa90ea1e6
sachnguyenbnk <sach.nguyen@bnksolution.com>
2025-05-16 11:31:22 +0700

    Update uninstall.php
    remove link

--- uninstall.php
+++ uninstall.php
@@ -19,7 +19,6 @@
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link       https://bnksolution.com
  * @since      1.0.1
  *
  * @package    Checkout_Confirm
